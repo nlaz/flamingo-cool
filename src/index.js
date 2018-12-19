75ad8295af322f3befea38597b33eea941f98af1
nlaz <nikolazaris@utexas.edu>
2018-12-19 17:07:07 -0500

    Support canceling events

--- src/index.js
+++ src/index.js
@@ -44,20 +44,21 @@ const createEvent = (userId, title, userName) => ({
 
 const createUsageMessage = (userId, channelId) => {
   axios
-  .post(
-    "https://slack.com/api/chat.postEphemeral",
-    qs.stringify({
-      token: process.env.SLACK_ACCESS_TOKEN,
-      channel: channelId,
-      as_user: true,
-      user: userId,
-      text: 'To create an invitation, try to formatting your message like this: \n `/whosin happy hour next week on wednesday 5pm-6pm`'
-    }),
+    .post(
+      "https://slack.com/api/chat.postEphemeral",
+      qs.stringify({
+        token: process.env.SLACK_ACCESS_TOKEN,
+        channel: channelId,
+        as_user: true,
+        user: userId,
+        text:
+          "To create an invitation, try to formatting your message like this: \n `/whosin happy hour next week on wednesday 5pm-6pm`",
+      }),
     )
-  .catch(err => {
-    console.error(err);
-  });
-}
+    .catch(err => {
+      console.error(err);
+    });
+};
 
 /*
  * Endpoint to receive /whosin slash command from Slack.
@@ -69,77 +70,75 @@ app.post("/whosin", (req, res) => {
   if (text.length > 0) {
     res.send({ response_type: "in_channel" });
   } else {
-    res.send('');
+    res.send("");
   }
 
   // Verify the signing secret
   if (signature.isVerified(req)) {
-
     if (text.length > 0) {
+      let gcalLink = "";
+      const event = createEvent(user_id, text, user_name);
 
-    let gcalLink = '';
-    const event = createEvent(user_id, text, user_name);
+      const feedbackLink = "https://goo.gl/forms/IY9t25qqNWYLgW9u2";
+      const parsedDate = chrono.parse(event.title)[0];
 
-    const feedbackLink = "https://goo.gl/forms/IY9t25qqNWYLgW9u2";
-    const parsedDate = chrono.parse(event.title)[0];
+      if (parsedDate) {
+        const startDate = parsedDate.start.date();
+        const oneHourAhead = new Date(startDate.getTime() + 1 * 60 * 60 * 1000);
+        const endDate = parsedDate.end ? parsedDate.end.date() : oneHourAhead;
+        const fmtStartDate = dateFormat(startDate, "UTC:yyyymmdd'T'HHMMss'Z'");
+        const fmtEndDate = dateFormat(endDate, "UTC:yyyymmdd'T'HHMMss'Z'");
 
-    if (parsedDate) {
-      const startDate = parsedDate.start.date();
-      const oneHourAhead = new Date(startDate.getTime() + 1 * 60 * 60 * 1000);
-      const endDate = parsedDate.end ? parsedDate.end.date() : oneHourAhead;
-      const fmtStartDate = dateFormat(startDate, "UTC:yyyymmdd'T'HHMMss'Z'");
-      const fmtEndDate = dateFormat(endDate, "UTC:yyyymmdd'T'HHMMss'Z'");
-
-      gcalLink = `http://www.google.com/calendar/event?${queryString.stringify({
-        action: "TEMPLATE",
-        text: event.title,
-        dates: `${fmtStartDate}/${fmtEndDate}`,
-      })}`;
-    }
-    axios
-      .post(
-        "https://slack.com/api/chat.postMessage",
-        qs.stringify({
-          token: process.env.SLACK_ACCESS_TOKEN,
-          channel: channel_id,
-          as_user: true,
-          attachments: JSON.stringify([
-            {
-              text: `You're invited to:\n *${event.title}*`,
-              color: "#6dc9da",
-            },
-            {
-              fallback: "You are unable to RSVP.",
-              color: "#6dc9da",
-              callback_id: "event_rsvp",
-              actions: [
-                {
-                  name: "yes",
-                  type: "button",
-                  text: ":tada: I'll be there",
-                  value: "yes",
-                },
-              ],
-            },
-            {
-              fallback: "No one is attending at the moment.",
-              color: "#6dc9da",
-              title: "Attending",
-              text: event.attending.map(el => `<@${el}>`).join(" "),
-            },
-            {
-              text: "",
-              color: "#6dc9da",
-              footer: `Who's In   <${feedbackLink}|Feedback>`,
-              footer_icon:
-                "https://avatars.slack-edge.com/2018-12-11/502563278519_39d786b2bb6ef9fbab5a_96.png",
-            },
-          ]),
-        }),
-      )
-      .catch(err => {
-        console.error(err);
-      });
+        gcalLink = `http://www.google.com/calendar/event?${queryString.stringify({
+          action: "TEMPLATE",
+          text: event.title,
+          dates: `${fmtStartDate}/${fmtEndDate}`,
+        })}`;
+      }
+      axios
+        .post(
+          "https://slack.com/api/chat.postMessage",
+          qs.stringify({
+            token: process.env.SLACK_ACCESS_TOKEN,
+            channel: channel_id,
+            as_user: true,
+            attachments: JSON.stringify([
+              {
+                text: `You're invited to:\n *${event.title}*`,
+                color: "#6dc9da",
+              },
+              {
+                fallback: "You are unable to RSVP.",
+                color: "#6dc9da",
+                callback_id: "event_rsvp",
+                actions: [
+                  {
+                    name: "yes",
+                    type: "button",
+                    text: ":tada: I'll be there",
+                    value: "yes",
+                  },
+                ],
+              },
+              {
+                fallback: "No one is attending at the moment.",
+                color: "#6dc9da",
+                title: "Attending",
+                text: event.attending.map(el => `<@${el}>`).join(" "),
+              },
+              {
+                text: "",
+                color: "#6dc9da",
+                footer: `Who's In    <${feedbackLink}|Feedback>`,
+                footer_icon:
+                  "https://avatars.slack-edge.com/2018-12-11/502563278519_39d786b2bb6ef9fbab5a_96.png",
+              },
+            ]),
+          }),
+        )
+        .catch(err => {
+          console.error(err);
+        });
     } else {
       // Message is empty.
       createUsageMessage(user_id, channel_id);
@@ -155,21 +154,61 @@ const DEFAULT_ATTENDING_MSG = ":see_no_evil: _No one is attending yet._";
 app.post("/response", (req, res) => {
   const { payload, ...rest } = req.body;
   res.send("");
-  const { channel, user, original_message, ts } = JSON.parse(req.body.payload);
+  const { channel, user, original_message, callback_id, message_ts, ts, message } = JSON.parse(
+    req.body.payload,
+  );
 
   const current_user = `<@${user.id}>`;
-  const previousTitle = original_message.attachments[0].text || '';
-  const previousAttending = original_message.attachments[2].text || "";
-  const attending = previousAttending === DEFAULT_ATTENDING_MSG ? [] : previousAttending.split(" ");
 
-  const updated =
-    attending.length > 0 && attending.indexOf(current_user) > -1
+  if (callback_id === "delete_event") {
+    // Deletes event
+    const previousTitle = message.attachments[0].text || "";
+    const title = previousTitle.substring(21, previousTitle.length - 1);
+
+    const previousAttending = message.attachments[2].text || "";
+    const attending =
+      previousAttending === DEFAULT_ATTENDING_MSG ? [] : previousAttending.split(" ");
+
+    axios.post(
+      "https://slack.com/api/chat.delete",
+      qs.stringify({
+        token: process.env.SLACK_ACCESS_TOKEN,
+        channel: channel.id,
+        as_user: true,
+        user: user.id,
+        ts: message_ts,
+      }),
+    );
+
+    attending.map(el => {
+      const userId = el.substring(2, el.length - 1);
+      axios.post(
+        "https://slack.com/api/chat.postEphemeral",
+        qs.stringify({
+          token: process.env.SLACK_ACCESS_TOKEN,
+          channel: channel.id,
+          as_user: true,
+          user: userId,
+          text: `${current_user} canceled the event: *${title}*`,
+        }),
+      );
+    });
+  } else if (callback_id === "event_rsvp") {
+    // Handles event response
+    const previousTitle = original_message.attachments[0].text || "";
+    const title = previousTitle.substring(21, previousTitle.length - 1);
+
+    const previousAttending = original_message.attachments[2].text || "";
+    const attending =
+      previousAttending === DEFAULT_ATTENDING_MSG ? [] : previousAttending.split(" ");
+
+    const isAlreadyGoing = attending.length > 0 && attending.indexOf(current_user) > -1;
+    const updated = isAlreadyGoing
       ? attending.filter(el => el !== current_user).join(" ")
       : [...attending, current_user].join(" ");
 
     let fmtStartDate;
     let fmtEndDate;
-    const title = previousTitle.substring(21, previousTitle.length - 1);
 
     const parsedDate = chrono.parse(title)[0];
 
@@ -182,59 +221,59 @@ app.post("/response", (req, res) => {
     }
 
     const gcalLink = `http://www.google.com/calendar/event?${queryString.stringify({
-        action: "TEMPLATE",
-        text: title,
-        dates: fmtStartDate && fmtEndDate ? `${fmtStartDate}/${fmtEndDate}` : undefined,
-      })}`;
+      action: "TEMPLATE",
+      text: title,
+      dates: fmtStartDate && fmtEndDate ? `${fmtStartDate}/${fmtEndDate}` : undefined,
+    })}`;
 
-  axios
-  .post(
-    "https://slack.com/api/chat.postEphemeral",
-    qs.stringify({
-      token: process.env.SLACK_ACCESS_TOKEN,
-      channel: channel.id,
-      as_user: true,
-      user: user.id,
-      text: 'Sweet you’re in! Add it to your calendar:',
-      attachments: JSON.stringify([
-            {
-              fallback: gcalLink,
-              color: "#6dc9da",
-              actions: [
-                {
-                  type: "button",
-                  text: "Add to calendar",
-                  url: gcalLink,
-                },
-              ],
-            },
-          ]),
-    }),
-    );
-  axios
-    .post(
-      "https://slack.com/api/chat.update",
+    axios.post(
+      "https://slack.com/api/chat.postEphemeral",
       qs.stringify({
         token: process.env.SLACK_ACCESS_TOKEN,
         channel: channel.id,
         as_user: true,
-        ts: original_message.ts,
+        user: user.id,
+        text: "Sweet you’re in! Add it to your calendar:",
         attachments: JSON.stringify([
-          original_message.attachments[0],
-          original_message.attachments[1],
           {
-            fallback: "No one is attending at the moment.",
+            fallback: gcalLink,
             color: "#6dc9da",
-            title: "Attending",
-            text: updated || DEFAULT_ATTENDING_MSG,
+            actions: [
+              {
+                type: "button",
+                text: "Add to calendar",
+                url: gcalLink,
+              },
+            ],
           },
-          original_message.attachments[3],
         ]),
       }),
-    )
-    .catch(err => {
-      console.error(err);
-    });
+    );
+    axios
+      .post(
+        "https://slack.com/api/chat.update",
+        qs.stringify({
+          token: process.env.SLACK_ACCESS_TOKEN,
+          channel: channel.id,
+          as_user: true,
+          ts: original_message.ts,
+          attachments: JSON.stringify([
+            original_message.attachments[0],
+            original_message.attachments[1],
+            {
+              fallback: "No one is attending at the moment.",
+              color: "#6dc9da",
+              title: "Attending",
+              text: updated || DEFAULT_ATTENDING_MSG,
+            },
+            original_message.attachments[3],
+          ]),
+        }),
+      )
+      .catch(err => {
+        console.error(err);
+      });
+  }
 });
 
 const server = app.listen(process.env.PORT || 5000, () => {
